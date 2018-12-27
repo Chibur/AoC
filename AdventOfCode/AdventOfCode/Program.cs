9846cbabd1508708c9cc589608d233be582930fc
chibur <edgarasciburys@gmail.com>
2018-12-27 20:54:25 +0200

    updated to .net core 3.0

--- AdventOfCode/AdventOfCode/Program.cs
+++ AdventOfCode/AdventOfCode/Program.cs
@@ -12,7 +12,7 @@ namespace AdventOfCode
             SolveDay2();
             SolveDay3();
             SolveDay4();
-            SolveDay5();
+            // SolveDay5();
         }
 
         static void SolveDay1()
